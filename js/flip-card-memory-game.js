5afb95cc0d53e4905d4ec0b780d97f5c9a0bcd67
Samantha Johnson <samantha.nisus@gmail.com>
2023-09-14 23:49:57 -0500

    added function to display card picture on onclick event

--- js/flip-card-memory-game.js
+++ js/flip-card-memory-game.js
@@ -15,17 +15,34 @@ class Card {
     }
 }
 
+//Create an array to store all the cards created, so we can use index of array to access individual card information
+const cards = [];
+
 //first set of cards
 const card1 = new Card(1, "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80");
+cards.push(card1); // add card to library of cards
 const card2 = new Card(1, "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80");
+cards.push(card2);// add card to library of cards
 
 //Second set of cards
 const card3 = new Card(2, "https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1942&q=80");
+cards.push(card3); // add card to library of cards
 const card4 = new Card(2, "https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1942&q=80");
+cards.push(card4); // add card to library of cards
 
 //third set of cards
 const card5 = new Card(3, "https://images.unsplash.com/photo-1596708803699-587668036834?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80");
+cards.push(card5); // add card to library of cards
 const card6 = new Card(3, "https://images.unsplash.com/photo-1596708803699-587668036834?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80");
+cards.push(card6); // add card to library of cards
 
 //example of how to check a card setNumber.
 console.log(`Card 1 set is ${card1.setNumberValue()}.`);
+
+//function to display card's picture
+function showCard(id){
+    //change div.style to show card's picture
+    //display picture with the same array index than the id parameter
+    //e.g div number 5 will show the picture of the card store at the index number 5 of the array cards
+    document.getElementById(id).setAttribute("style", `background: url('${cards[id].url}'); background-size: cover`);
+}
