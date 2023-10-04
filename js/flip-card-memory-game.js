beb5fd87135509030e7963adf9218992ece3c45e
Li Mattson <liamattson@gmail.com>
2023-10-04 14:53:33 -0600

    added stopwatch stop and match checker

--- js/flip-card-memory-game.js
+++ js/flip-card-memory-game.js
@@ -7,7 +7,7 @@ class Card {
         this.setNumber = setNumber;
         this.url = url;
     }
-    
+
     //This function return the Object's value of the property setNumber.
     //Will be used to check if 2 cards are from the same set.
     setNumberValue() {
@@ -18,9 +18,7 @@ class Card {
 //GLOBAL VARIABLES
 const CARDS = []; //Create an array to store all the cards created, so we can use index of array to access individual card information
 let CARD_SELECTED = []; //Array used to log player card id picked
-const SETS_FOUND = []; //Array used to keep track of card set found by player
-const NUM_TRIES =[]; //Array used to keep track of player attempts
-const SCORE = 0; //Variable keeping track of player's score
+const NUM_CARDS = 6; //Number of cards used in the game
 let stopwatchInterval; //Every second it refreseshes time that has elapsed
 let startTime; //The time at which the stopwatch starts
 let elapsedTime = 0; //Start elapsed time
@@ -49,30 +47,30 @@ const card6 = new Card(3, "https://images.unsplash.com/photo-1596708803699-58766
 CARDS.push(card6); // add card to library of cards
 
 //example of how to check a card setNumber.
-console.log(`card3 is part of set number ${card3.setNumberValue()}.`);
-console.log(`card4 is part of set number ${card4.setNumberValue()}.`);
+//console.log(`card3 is part of set number ${card3.setNumberValue()}.`);
+//console.log(`card4 is part of set number ${card4.setNumberValue()}.`);
 
 //Adding onclick events to each div card 
-for(i = 1; i < 7; i++) {
+for (i = 1; i < 7; i++) {
     let element = document.querySelector(`#card${i}`);
     let elementID = element.id;
-    let showCardFunction = () => showCard(elementID); 
+    let showCardFunction = () => showCard(elementID);
     element.addEventListener('click', function () { startStopwatch(); });
     //memorize function by adding it to array
     showCardFunctionsArr.push(showCardFunction); //even function is the same, considered different methods by javascript
     element.addEventListener('click', showCardFunction);
-   
+
 }
 
 //stopwatch functions
 function updateStopwatchDisplay() {
-	const stopwatchElement = document.getElementById('stopwatch');
-	const currentTime = new Date().getTime();
-	const timeDifference = new Date (currentTime - startTime + elapsedTime);
-	const minutes = timeDifference.getMinutes();
-	const seconds = timeDifference.getSeconds();
+    const stopwatchElement = document.getElementById('stopwatch');
+    const currentTime = new Date().getTime();
+    const timeDifference = new Date(currentTime - startTime + elapsedTime);
+    const minutes = timeDifference.getMinutes();
+    const seconds = timeDifference.getSeconds();
     const formattedTime = `${minutes}:${seconds.toString().padStart(2, '0')}`;
-	stopwatchElement.textContent = formattedTime;
+    stopwatchElement.textContent = formattedTime;
 }
 
 //starts stopwatch
@@ -82,18 +80,22 @@ function startStopwatch() {
         stopwatchInterval = setInterval(updateStopwatchDisplay, 1000);
     }
 }
-//startStopwatch();
+
+//stop stopwatch
+function stopStopwatch() {
+    clearInterval(stopwatchInterval);
+}
 
 
 //function to display card's picture
-function showCard(id){
+function showCard(id) {
 
     let cardElement = document.querySelector(`#${id}`);
     //create images element and add related src to element
     theImgContainer = document.querySelector(`#${id} > .flip-card-back`);
     let theImg = document.createElement('img');
-    idNumber = id[id.length-1]; //get the number part of the card id, which is last character
-    arrIdx = idNumber-1;
+    idNumber = id[id.length - 1]; //get the number part of the card id, which is last character
+    arrIdx = idNumber - 1;
     theImg.src = CARDS[arrIdx].url; //card5 picture is in array index 4, 5-1=4
 
     //add img element to DOM
@@ -104,58 +106,58 @@ function showCard(id){
 
     //add card to CARD_SELECTED array as a card selected by user
     CARD_SELECTED.push(id);
-    
+
     //display card
     cardElement.classList.add('flipped-card-inner');
 
     //display in console card selected
-    console.log(id); 
+   // console.log(id);
 
     //if 2 cards are selected, hide both cards 
     if (CARD_SELECTED.length === 2) {
         const [cardId1, cardId2] = CARD_SELECTED; // Get the IDs of the selected cards
-        console.log('cardId1', cardId1);
-        console.log('cardId2', cardId2);
         const card1 = document.querySelector(`#${cardId1}`);
         const card2 = document.querySelector(`#${cardId2}`);
-    
-        // Check if both cards are defined and have the dataset property
-        if (card1 && card2 && card1.dataset && card2.dataset) {
-            // Check if the 'card' property exists in the dataset
-            const card1Data = card1.dataset.card;
-            const card2Data = card2.dataset.card;
 
-    
-            if (card1.setNumber === card2.setNumber) {
-                // Cards match
-                card1.classList.add('hidden');
-                card2.classList.add('hidden');
-    
-            } else {
-                // Cards do not match
-                // Delay the call to hideCards function by 1 second - 1000 milliseconds
-                setTimeout(() => {
-                    hideCards();
-                }, 1000);
-            }
+        if (CARDS[card1.dataset.index].setNumberValue() === CARDS[card2.dataset.index].setNumberValue()) {
+            // Cards match
+            //console.log('cards matched!')
+            allCardsMatched();
+            CARD_SELECTED = [];
+
+
         } else {
-            // Log an error or handle the case where cards are not properly defined
-            console.error("Error: One or both cards are not properly defined.");
+            // Cards do not match
+            // Delay the call to hideCards function by 1 second - 1000 milliseconds
+            setTimeout(() => {
+                hideCards();
+            }, 1000);
         }
-    
+
         numTries++; // Increment the number of tries
         document.querySelector('#increment').textContent = numTries; // Update the display with the number of tries
+
     }
 }
 
+//checking if all cards are displayed
+function allCardsMatched() {
+    const allCardsElement = document.querySelectorAll('.flipped-card-inner');
+    if (allCardsElement.length === NUM_CARDS) {
+        stopStopwatch();
+    }
+
+}
+
+
 function hideCards() {
     for (let card of CARD_SELECTED) {
-        let theCard = document.querySelector(`#${card}`); 
-        theCard.classList.remove('flipped-card-inner'); 
-        
+        let theCard = document.querySelector(`#${card}`);
+        theCard.classList.remove('flipped-card-inner');
+
         //restore onclick event
-        idNumber = card[card.length-1]; //get the number part of the card id, which is last character
-        arrIdx = idNumber-1;
+        idNumber = card[card.length - 1]; //get the number part of the card id, which is last character
+        arrIdx = idNumber - 1;
         theCard.addEventListener('click', showCardFunctionsArr[arrIdx]);
 
         //delete img element
